8c56b01dd780b4f93ccf9bd40be60c8a6c647b7e
Benjamin Morgan (RIT Student) <bxm8563@ad.rit.edu>
2019-04-10 12:47:18 -0400

    Wheel puzzle complete
    will need adjustments to match vr height/reach

---
+++ VRm v2/Assets/ScrambleGroup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrambleGroup : MonoBehaviour
+{
+
+    public static Action IncrementSolved;
+    private int nSolved = 0;
+
+    [SerializeField]
+    private List<GameObject> scramblePieces;
+    [SerializeField]
+    private GameObject solvedWheel;
+
+
+    private void Awake()
+    {
+        IncrementSolved += Increment;
+    }
+
+    private void OnDestroy()
+    {
+        IncrementSolved -= Increment;
+    }
+
+    private void Increment()
+    {
+        nSolved++;
+        if(nSolved == scramblePieces.Count)
+        {
+            solvedWheel.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+}
