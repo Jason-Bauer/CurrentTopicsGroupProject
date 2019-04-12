8eac52f7cb6700b5240f9b2480077d676ca74cea
JimLicata <jimmays.jl@gmail.com>
2019-04-12 11:56:09 -0400

    added chest

---
+++ VRm v2/Assets/Scriptes/Chest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    bool open = false;
+    GameObject hinge;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hinge = GameObject.Find("Hinge");
+        open = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(open)
+        {
+            hinge.transform.Rotate(0f, 0f, hinge.transform.rotation.z + 1);
+        }
+    }
+
+    void Open()
+    {
+        
+        
+    }
+}
