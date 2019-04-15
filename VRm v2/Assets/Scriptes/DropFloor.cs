682f18402fcaf60df4db7da2b43eac41fbc717a3
Jason-Bauer <jcb5944@g.rit.edu>
2019-04-15 11:38:47 -0400

    DropFloor Working

---
+++ VRm v2/Assets/Scriptes/DropFloor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropFloor : MonoBehaviour
+{
+    public GameObject floor;
+    private IEnumerator coroutine;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Drop()
+    {
+        floor.SetActive(false);
+        coroutine = Comeback();
+        StartCoroutine(coroutine);
+
+    }
+    IEnumerator Comeback()
+    {
+
+        yield return new WaitForSeconds(4);
+        floor.SetActive(true);
+    }
+}
