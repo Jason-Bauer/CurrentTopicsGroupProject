7c9801acab0e56b731fed14f23e16660d2fd34cb
zanthous <bcm023@gmail.com>
2019-04-19 11:58:24 -0400

    Experimenting with new mechanics

--- VRm v2/Assets/TransportPlayer.cs
+++ VRm v2/Assets/TransportPlayer.cs
@@ -17,7 +17,17 @@ public class TransportPlayer : MonoBehaviour
 
     public SteamVR_Action_Boolean grabPinch; //Grab Pinch is the trigger, select from inspector
     public SteamVR_Input_Sources inputSource = SteamVR_Input_Sources.Any;//which controller
-                                                                         // Use this for initialization
+
+    private float timer = 0.0f;
+    private float duration;
+
+    private Vector3 startPos;
+    private Vector3 startRot;
+
+    [SerializeField]
+    private bool debug;
+    [SerializeField]
+    private GameObject debugPlayer;
 
     void OnEnable()
     {
@@ -32,7 +42,13 @@ public class TransportPlayer : MonoBehaviour
         if(newState)
         {
             //start coroutine lerp player transform to target transform
+            StartCoroutine(LerpPlayer());
         }
+    }
+
+    private void OnMouseDown()
+    {
+        StartCoroutine(LerpPlayer());
     }
 
     private void OnDisable()
@@ -46,14 +62,30 @@ public class TransportPlayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        var leftHandDevices = new List<UnityEngine.XR.InputDevice>();
-        UnityEngine.XR.InputDevices.GetDeviceAtXRNode(UnityEngine.XR.XRNode.LeftEye);
-
         if (player == null)
             GameObject.FindGameObjectWithTag("Player");
+        if (debug)
+            player = debugPlayer;
     }
 
+    IEnumerator LerpPlayer()
+    {
+        duration = Vector3.Distance(player.transform.position, targetTransform.transform.position);
+        duration /= 8.0f;
+        startPos = player.transform.position;
+        startRot = player.transform.rotation.eulerAngles;
+        while (timer < duration)
+        {
+            timer += Time.deltaTime;
+            rPos = Vector3.Lerp(startPos, targetTransform.transform.position, timer / duration);
+            rRot = Vector3.Lerp(startRot, targetTransform.transform.rotation.eulerAngles, timer / duration);
+            player.transform.position = rPos;
+            player.transform.eulerAngles = rRot;
+            yield return null;
+        }
 
+        timer = 0.0f;
+    }
 
 
 }
