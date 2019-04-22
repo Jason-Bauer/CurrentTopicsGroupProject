459ed48843dd68a89ba9df31547acd1e9d4a7308
Jason <a>
2019-04-22 11:39:30 -0400

    Scenes Merged

--- VRm v2/Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ VRm v2/Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -97,6 +97,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Vector2 Turn
+        {
+            get
+            {
+                return SteamVR_Actions.default_Turn;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
