659724eb1fcb1b5cbef8deab8401160a5ccef67a
Jason <a>
2019-04-26 11:41:54 -0400

    Gun working

--- VRm v2/Assets/Scriptes/Gun.cs
+++ VRm v2/Assets/Scriptes/Gun.cs
@@ -1,25 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Gun : MonoBehaviour
+namespace Valve.VR.InteractionSystem
 {
-    public GameObject bullet;
-    public GameObject bulletholder;
-    // Start is called before the first frame update
-    void Start()
+    public class Gun : MonoBehaviour
     {
-        
-    }
+        public GameObject bullet;
+        public GameObject bulletholder;
+        public bool reset = false;
+        // Start is called before the first frame update
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    public void shoot()
-    {
-        GameObject b1 = Instantiate(bullet, bulletholder.transform);
-        b1.transform.position = this.gameObject.transform.position;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (this.gameObject.GetComponent <LinearMapping>().value>=.8 && !reset)
+            {
+                shoot();
+                reset = true;
+            }
+            else if(reset && this.gameObject.GetComponent<LinearMapping>().value <= .5)
+            {
+                reset = false;
+            }
+        }
+        public void shoot()
+        {
+            GameObject b1 = Instantiate(bullet, bulletholder.transform);
+            b1.transform.position = this.gameObject.transform.position;
+            b1.GetComponent<Bullet>().forward = this.transform.right;
+        }
     }
 }
